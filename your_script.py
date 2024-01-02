5f9cab7f928a73576c4d51ac8247fb465decc567
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2024-01-02 17:06:11 +0530

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -4,8 +4,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 # Read data from Excel file
-excel_file_path = 'PLOTTING_DATA_AVG_DIST_DISTRIBUTION.xlsx'
-df = pd.read_excel(excel_file_path, index_col='Medicine Name', engine='xlrd')
+excel_file_path = 'PLOTTING_DATA_AVG_DIST_DISTRIBUTION.csv'
+df = pd.read_csv(excel_file_path, index_col='Medicine Name')
 
 # Function for creating the bar plot
 def MED_BAR_PLOT(dataframe, row_name):
